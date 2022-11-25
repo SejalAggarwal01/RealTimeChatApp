146753dbc729ec1a03560f2217c9864ba8f7b3d0
Sejal Aggarwal <sejalaggarwal101@gmail.com>
2022-11-25 20:23:36 +0530

    Initial commit

---
+++ js/client.js
@@ -0,0 +1,7 @@
+const io = require('socket.io-client')(8000)
+const socket = io.connect('http://localhost:8000');
+const form = document.getElementById('form-container')
+const messageInput = document.getElementById('messageInp')
+const messageContainer = document.querySelector(".message-container")
+const names = prompt("Enter your name to join");
+socket.emit('if-new-user-joined', names)
