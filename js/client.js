d8f67b4c7bbc662f593cc1eb110c0320dc0490bf
Sejal Aggarwal <sejalaggarwal101@gmail.com>
2022-12-19 17:51:03 +0530

    Almost Done

--- js/client.js
+++ js/client.js
@@ -1,7 +1,36 @@
 // const io = require('socket.io-client')(8000)
 const socket = io.connect('http://localhost:8000');
-const form = document.getElementById('form-container')
+
+const form = document.getElementById('send-container')
 const messageInput = document.getElementById('messageInp')
-const messageContainer = document.querySelector(".message-container")
+const messageContainer = document.querySelector(".container")
+var audio=new Audio('Pikachu Notification.mp3')
+
+const append=(message,position)=>{
+    const messageElement=document.createElement('div');
+    messageElement.innerText=message;
+    messageElement.classList.add('message')
+    messageElement.classList.add(position)
+    messageContainer.append(messageElement)
+    audio.play();
+}
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const message=messageInput.value;
+    append(`You:${message}`,'right')
+    socket.emit('send',message)
+    messageInput.value=''
+})
 const names = prompt("Enter your name to join");
-socket.emit('if-new-user-joined', names)
+socket.emit('new-user-joined', names)
+
+socket.on('user-joined',names=>{
+    append(`${names} joined the chat`,'left')
+})
+socket.on('receive',data=>{
+    append(`${data.names}: ${data.message}`,'left')
+})
+socket.on('left',name=>{
+    append(`${data.name} left the chat`,'left')
+})
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -3,11 +3,16 @@ const io = require('socket.io')(8000)
 const users = {};
 
 io.on('connection', socket => {
-    socket.on('if-new-user-joined', name => {
-        users[socket.id] = name;
-        socket.broadcast.emit('user-joined', name)
+    socket.on('new-user-joined', names => {
+        // console.log("hi",names)
+        users[socket.id] = names;
+        socket.broadcast.emit('user-joined', names)
     });
-    socket.on('if-send', message => {
-        socket.broadcast.emit('receive', { message: message, name: users[socket.id] })
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', { message: message, names: users[socket.id] })
+    });
+    socket.on('disconnect', message => {
+        socket.broadcast.emit('left', users[socket.id] )
+        delete users[socket.id]
     });
 })
